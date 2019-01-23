01dcd5bda5813572581512f3a02330aefecfc1c9
Franck Lucien Duriez <franck@keecker.com>
2019-01-23 20:29:01 +0100

    Move ProjectionConstants here.

---
+++ src/main/java/com/keecker/services/projection/interfaces/ProjectionConstants.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (C) 2015 KEECKER SAS (www.keecker.com)
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ */
+package com.keecker.services.projection.interfaces;
+
+/**
+ * Constants for min/max value of projector settings
+ */
+public class ProjectionConstants {
+    public static final int MIN_KEYSTONE = -40;
+    public static final int MAX_KEYSTONE = 40;
+    public static final int MIN_BRIGHTNESS = 0;
+    public static final int MAX_BRIGHTNESS = 100;
+    public static final int MIN_ZOOM = 0;
+    public static final int MAX_ZOOM = 100;
+    public static final int MIN_CONTRAST = 0;
+    public static final int MAX_CONTRAST = 100;
+    public static final int MIN_ORIENTATION = 0;
+    public static final int DEFAULT_ORIENTATION = 10;
+    public static final int MAX_ORIENTATION = 78;
+    public static final int MIN_FOCUS = 0;
+    public static final int MAX_FOCUS = 100;
+}
